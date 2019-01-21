479b29f823f2698791c7b01ce3c1113bfc6ec6f7
SWING1993 <songguohua10446627@gmail.com>
2019-01-21 11:49:09 +0800

    update

--- src/main/java/com/swing/orange/OrangeApplication.java
+++ src/main/java/com/swing/orange/OrangeApplication.java
@@ -7,24 +7,28 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 
 
-@SpringBootApplication
-public class OrangeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(OrangeApplication.class, args);
-	}
-
-}
-
-///**
-// * 修改启动类，继承 SpringBootServletInitializer 并重写 configure 方法
-// */
 //@SpringBootApplication
-//public class OrangeApplication extends SpringBootServletInitializer {
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//        // 注意这里要指向原先用main方法执行的Application启动类
-//        return builder.sources(OrangeApplication.class);
-//    }
+//public class OrangeApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(OrangeApplication.class, args);
+//	}
+//
 //}
+
+/**
+ * 修改启动类，继承 SpringBootServletInitializer 并重写 configure 方法
+ */
+@SpringBootApplication
+public class OrangeApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(OrangeApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrangeApplication.class, args);
+    }
+}
 
