a91236f3f8e9a8121a6eb705efa21471dd560cff
SWING1993 <songguohua10446627@gmail.com>
2019-01-22 16:38:24 +0800

    update

--- src/main/java/com/swing/orange/ServletInitializer.java
+++ src/main/java/com/swing/orange/ServletInitializer.java
@@ -3,7 +3,7 @@ package com.swing.orange;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class SpringBootStartApplication extends SpringBootServletInitializer {
+public class ServletInitializer extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(OrangeApplication.class);
