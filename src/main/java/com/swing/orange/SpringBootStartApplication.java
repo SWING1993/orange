636ec352dbd69692b0a4092aeea31a01de3b7ed2
SWING1993 <songguohua10446627@gmail.com>
2019-01-22 09:08:11 +0800

    update

--- src/main/java/com/swing/orange/SpringBootStartApplication.java
+++ src/main/java/com/swing/orange/SpringBootStartApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class SpringBootStartApplication  extends SpringBootServletInitializer {
-
-
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(OrangeApplication.class);
