05035a4294a99a6491a0de27a18e1c935467be84
SWING1993 <songguohua10446627@gmail.com>
2019-01-26 16:02:55 +0800

    update

--- src/test/java/com/swing/orange/MailServiceTest.java
+++ src/test/java/com/swing/orange/MailServiceTest.java
@@ -11,21 +11,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MailServiceTest {
 
-    @Autowired
-    private MailService mailService;
-
-    @Test
-    public void testSimpleMail() throws Exception {
-        mailService.sendSimpleMail("admin@swing1993.cn","test simple mail"," hello this is simple mail");
-    }
-
-    @Test
-    public void testHtmlMail() throws Exception {
-        String content="<html>\n" +
-                "<body>\n" +
-                "    <h3>hello world ! 这是一封Html邮件!</h3>\n" +
-                "</body>\n" +
-                "</html>";
-        mailService.sendHtmlMail("admin@swing1993.cn", "test html mail", content);
-    }
+//    @Autowired
+//    private MailService mailService;
+//
+//    @Test
+//    public void testSimpleMail() throws Exception {
+//        mailService.sendSimpleMail("admin@swing1993.cn","test simple mail"," hello this is simple mail");
+//    }
+//
+//    @Test
+//    public void testHtmlMail() throws Exception {
+//        String content="<html>\n" +
+//                "<body>\n" +
+//                "    <h3>hello world ! 这是一封Html邮件!</h3>\n" +
+//                "</body>\n" +
+//                "</html>";
+//        mailService.sendHtmlMail("admin@swing1993.cn", "test html mail", content);
+//    }
 }
